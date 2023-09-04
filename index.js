d0f57e67647baa1891c94498cdf419cc395c6fd1
Marian <mocanumarian1993@gmail.com>
2023-09-04 19:42:52 +0200

    styling

--- index.js
+++ index.js
@@ -14,28 +14,45 @@ const generalErrorBody =
   "We apologize, but an unexpected error has occurred while processing your request. Our technical team has been notified of this issue, and we are working diligently to resolve it. In the meantime, you can try refreshing the page and attempt your action again.";
 
 window.onload = () => {
-  const form = document.getElementById("contact-form");
-  const modal = document.getElementById("modal");
-  const closeModalBtn = document.getElementById("closeModalBtn");
-  const modalTitle = document.getElementById("modal-title");
-  const modalContent = document.getElementById("modal-content");
+  // removing whitespaces
+  const nameInput = document.getElementById("name");
+  nameInput.addEventListener("blur", () => (nameInput.value = nameInput.value.trim()));
+  const emailInput = document.getElementById("email");
+  emailInput.addEventListener("blur", () => (emailInput.value = emailInput.value.trim()));
+  const messageInput = document.getElementById("message");
+  messageInput.addEventListener("blur", () => (messageInput.value = messageInput.value.trim()));
 
+  const resetForm = () => {
+    nameInput.value = "";
+    emailInput.value = "";
+    messageInput.value = "";
+  };
+
+  // date validation
   const isDateValid = () => {
     const today = new Date();
     const dayOfWeek = today.getDay();
     return dayOfWeek !== 5 && dayOfWeek !== 6 && dayOfWeek !== 0;
   };
 
+  // email validation
   const isEmailValid = () => {
     const email = document.getElementById("email").value.trim();
     const pattern = /@cphbusiness\.dk$/i;
     return pattern.test(email);
   };
 
+  // message validation
   const isMessageValid = () => {
     const message = document.getElementById("message").value.trim();
     return message.length > 7;
   };
+
+  const form = document.getElementById("contact-form");
+  const modal = document.getElementById("modal");
+  const closeModalBtn = document.getElementById("closeModalBtn");
+  const modalTitle = document.getElementById("modal-title");
+  const modalContent = document.getElementById("modal-content");
 
   closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
@@ -75,11 +92,13 @@ window.onload = () => {
         modal.style.display = "flex";
         modalTitle.textContent = successTitle;
         modalContent.textContent = successBody;
+        resetForm();
       },
       function (error) {
         modal.style.display = "flex";
         modalTitle.textContent = errorTitle;
         modalContent.textContent = generalErrorBody;
+        console.log("Error", error);
       }
     );
   });
